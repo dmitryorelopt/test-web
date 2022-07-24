a7f2968d57fec5a9bae34ca886b591175efd80e2
Dmitry Orel <dmitry.orel@av.by>
2022-07-24 12:48:41 +0400

    add socket io

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,49 @@
+import React, { MouseEventHandler, useEffect, useState } from 'react';
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { io, Socket } from "socket.io-client";
+
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [socket, setSocket] = useState<Socket>();
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    const newSocket = io('http://localhost:3000');
+
+    newSocket.on('connect', () => {
+      setIsConnected(true);
+    });
+
+    newSocket.on('exception', function(data) {
+      console.log('event', data);
+    });
+
+    newSocket.on('disconnect', function() {
+      console.log('Disconnected');
+
+      setIsConnected(false);
+    });
+
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.off('connect');
+      newSocket.off('disconnect');
+      newSocket.off('pong');
+    };
+  }, []);
+
+  const sendFindAll: MouseEventHandler<HTMLAnchorElement> = (e) => {
+    e.preventDefault();
+
+    socket?.emit('findAllUsers', (response: string) => {
+      console.log(response);
+    });
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,7 +54,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Connected: {isConnected ? 'Yes' : 'No'}
         </h1>
 
         <p className={styles.description}>
@@ -23,32 +63,9 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href="https://nextjs.org/docs" className={styles.card} onClick={sendFindAll}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
           </a>
         </div>
       </main>
